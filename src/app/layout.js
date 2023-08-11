8884b47f6325afe5259805a63f3530601206832f
Telnesito <carlosj_1703@hotmail.com>
2023-08-10 22:35:47 -0400

    icon-page

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" href="images/maleta.png" />
       <body className={inter.className}>{children}
       </body>
 
